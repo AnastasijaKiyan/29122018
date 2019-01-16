297355a059cbbccfd504c6cd5659d97d876cb281
mordenius <mordenius@gmail.com>
2019-01-16 16:21:07 +0200

    format(type) remove unused

--- src/type/IReduser.tsx
+++
@@ -1,7 +0,0 @@
-import { Reducer } from "redux";
-import { IAction } from './IAction';
-import { IState } from './IState';
-
-export interface IReducer extends Reducer {
-  (state: IState, action: IAction): IState;
-}
